5a97c506bbda5458a0663eb47f2b930ef9006a6e
martin <telemagico@gmail.com>
2019-10-11 09:04:46 -0300

    distancematrix test

--- server.js
+++ server.js
@@ -73,19 +73,15 @@ mongodb.MongoClient.connect(process.env.MONGO_URL, {useNewUrlParser: true }, fun
     res.render('index')
   });
 
-  app.get('/test-distancematrix2', function (req, res) {  
-    return axios.get( 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=13.13212951,80.24748131&destinations=12.9593040,80.2450580&mode=driving&key=' + process.env.API_KEY, {} )
-  })
-
   app.get('/test-distancematrix', function (req, res) {  
     return axios.get( 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=13.13212951,80.24748131&destinations=12.9593040,80.2450580&mode=driving&key=' + process.env.API_KEY, {} ).then((response) => {
-      return res.json(response)
+      return res.json(response.data)
     })        
   })
 
   app.post('/distancematrix', function (req, res) {  
-    axios.get( 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + req.body.from.lat + ',' + req.body.from.lng + '&destinations=' + req.body.to.lat + ',' + req.body.to.lng + '&mode=driving&key=' + process.env.API_KEY, {} ).then((response) => {
-      return res.json(response)
+    return axios.get( 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + req.body.from.lat + ',' + req.body.from.lng + '&destinations=' + req.body.to.lat + ',' + req.body.to.lng + '&mode=driving&key=' + process.env.API_KEY, {} ).then((response) => {
+      return res.json(response.data)
     })        
   })
 
