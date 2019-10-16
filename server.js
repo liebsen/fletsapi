eba80ddc0a04b881531620f16ffc03c167a4530c
martin <telemagico@gmail.com>
2019-10-16 13:57:21 -0300

    find test

--- server.js
+++ server.js
@@ -233,11 +233,11 @@ mongodb.MongoClient.connect(process.env.MONGO_URL, {useNewUrlParser: true }, fun
 
   app.get('/testfind', function (req, res) { 
 
-    db.collection('notifications').find({id:5328939178}, function(err, doc){
+    db.collection('notifications').find({id:5328939178}).toArray(function(err, doc) {
       console.log("1")
       console.log(doc.val)
 
-      db.collection('notifications').find({id:5328}, function(err, doc2){
+      db.collection('notifications').find({id:5328}).toArray(function(err, doc2) {
         console.log("2")
         console.log(doc2.val)
       })
