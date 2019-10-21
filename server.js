54a3fca1e0be960db7b4750281dfda8046e1bbf8
martin <telemagico@gmail.com>
2019-10-21 16:05:57 -0300

    cors update

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const allowedOrigins = [
   'http://localhost:4000',
   'https://localhost:8080',
   'https://fletsapp.herokuapp.com',
+  'https://fletspanel.herokuapp.com',
   'https://fletsapi.herokuapp.com',
   'https://fletsapp.com'  
 ]
@@ -49,6 +50,8 @@ app.use(cors({
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
