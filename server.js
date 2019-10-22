b3daf7adb262a2eee386f201059a280b1bd17cd5
martin <telemagico@gmail.com>
2019-10-22 10:08:53 -0300

    debug not allowed origins

--- server.js
+++ server.js
@@ -37,7 +37,10 @@ app.use(cors({
   origin: function(origin, callback){
     // allow requests with no origin 
     // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true)
+    if(!origin) {
+      console.log(origin)
+      return callback(null, true)
+    }
     if(allowedOrigins.indexOf(origin) === -1){
       var msg = 'The CORS policy for this site does not ' +
                 'allow access from the specified Origin.'
